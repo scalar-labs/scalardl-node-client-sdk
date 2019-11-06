5613c0fdf826def84a1cc091c557d339c9105334
moon004 <herodotus94@gmail.com>
2019-11-06 11:23:29 +0900

    Removed unwanted files and update tests

--- test/integration_test.js
+++ test/integration_test.js
@@ -9,7 +9,7 @@ const properties = {
   'scalar.ledger.client.server_host': 'localhost',
   'scalar.ledger.client.server_port': 50051,
   'scalar.ledger.client.server_privileged_port': 50052,
-  'scalar.ledger.client.cert_holder_id': 'foo@example.com',
+  'scalar.ledger.client.cert_holder_id': `foo@${Date.now()}`, // Make the test idempotent.
   'scalar.ledger.client.private_key_pem': '-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEICcJGMEw3dyXUGFu/5a36HqY0ynZi9gLUfKgYWMYgr/IoAoGCCqGSM49\nAwEHoUQDQgAEBGuhqumyh7BVNqcNKAQQipDGooUpURve2dO66pQCgjtSfu7lJV20\nXYWdrgo0Y3eXEhvK0lsURO9N0nrPiQWT4A==\n-----END EC PRIVATE KEY-----\n',
   'scalar.ledger.client.cert_pem': '-----BEGIN CERTIFICATE-----\nMIICizCCAjKgAwIBAgIUMEUDTdWsQpftFkqs6bCd6U++4nEwCgYIKoZIzj0EAwIw\nbzELMAkGA1UEBhMCSlAxDjAMBgNVBAgTBVRva3lvMQ4wDAYDVQQHEwVUb2t5bzEf\nMB0GA1UEChMWU2FtcGxlIEludGVybWVkaWF0ZSBDQTEfMB0GA1UEAxMWU2FtcGxl\nIEludGVybWVkaWF0ZSBDQTAeFw0xODA5MTAwODA3MDBaFw0yMTA5MDkwODA3MDBa\nMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJ\nbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNC\nAAQEa6Gq6bKHsFU2pw0oBBCKkMaihSlRG97Z07rqlAKCO1J+7uUlXbRdhZ2uCjRj\nd5cSG8rSWxRE703Ses+JBZPgo4HVMIHSMA4GA1UdDwEB/wQEAwIFoDATBgNVHSUE\nDDAKBggrBgEFBQcDAjAMBgNVHRMBAf8EAjAAMB0GA1UdDgQWBBRDd2MS9Ndo68PJ\ny9K/RNY6syZW0zAfBgNVHSMEGDAWgBR+Y+v8yByDNp39G7trYrTfZ0UjJzAxBggr\nBgEFBQcBAQQlMCMwIQYIKwYBBQUHMAGGFWh0dHA6Ly9sb2NhbGhvc3Q6ODg4OTAq\nBgNVHR8EIzAhMB+gHaAbhhlodHRwOi8vbG9jYWxob3N0Ojg4ODgvY3JsMAoGCCqG\nSM49BAMCA0cAMEQCIC/Bo4oNU6yHFLJeme5ApxoNdyu3rWyiqWPxJmJAr9L0AiBl\nGc/v+yh4dHIDhCrimajTQAYOG9n0kajULI70Gg7TNw==\n-----END CERTIFICATE-----\n',
   'scalar.ledger.client.cert_version': 1,
@@ -20,7 +20,7 @@ let clientService;
 
 describe('Integration test on ClientService', async () => {
   const mockedFunctionId = 'TestFunction';
-  const mockedContractId = 'StateUpdater';
+  const mockedContractId = `StateUpdater${Date.now()}`;
   const mockedContractName = 'com.org1.contract.StateUpdater';
   const mockedFunctionName = 'com.org1.function.TestFunction';
   const mockedAssetId = 'mockedAssetId';
@@ -57,7 +57,6 @@ describe('Integration test on ClientService', async () => {
       const response = await clientService.registerContract(mockedContractId,
           mockedContractName,
           mockedByteContract, property);
-      console.log(response);
       assert.equal(response.getStatus(), 200);
     });
   });
@@ -67,18 +66,16 @@ describe('Integration test on ClientService', async () => {
       assert.equal(response.getStatus(), 200);
     });
   });
-  describe('validateLedger', () => {
-    it('should works as expected', async () => {
-      const response = await clientService.validateLedger(mockedAssetId);
-      console.log(response);
-      assert.equal(response.getStatus(), 200);
-    });
-  });
   describe('executeContract', () => {
     it('should works as expected', async () => {
       const response = await clientService.executeContract(mockedContractId,
           mockedArgument, property);
-      console.log(response);
+      assert.equal(response.getStatus(), 200);
+    });
+  });
+  describe('validateLedger', () => {
+    it('should works as expected', async () => {
+      const response = await clientService.validateLedger(mockedAssetId);
       assert.equal(response.getStatus(), 200);
     });
   });
