e119dd687c1c0ed5ee3a3b3d8a945aea70176011
scalarindetail <40993647+scalarindetail@users.noreply.github.com>
2019-11-14 12:00:32 +0900

    Feature/integration test (#8)

--- scalardl-node-client-sdk.js
+++ scalardl-node-client-sdk.js
@@ -4,7 +4,7 @@ const {
 } = require('@scalar-labs/scalardl-javascript-sdk-base');
 
 const protobuf = require('./scalar_pb');
-const {LedgerClient} = require('./scalar_grpc_pb');
+const {LedgerClient, LedgerPrivilegedClient} = require('./scalar_grpc_pb');
 const grpc = require('grpc');
 
 /**
@@ -16,19 +16,31 @@ class ClientService extends ClientServiceBase {
    * @param {Object} properties
    */
   constructor(properties) {
-    const url = `${properties['scalar.ledger.client.server_host']}:` +
-      `${properties['scalar.ledger.client.server_port']}`;
+    const ledgerClientUrl = `${properties['scalar.ledger.client.server_host']}:` +
+        `${properties['scalar.ledger.client.server_port']}`;
+    const ledgerPrivilegedClientUrl = `${properties['scalar.ledger.client.server_host']}:` +
+        `${properties['scalar.ledger.client.server_privileged_port']}`;
     const ca = properties['scalar.ledger.client.tls.ca_root_cert'];
     const tlsEnabled = properties['scalar.ledger.client.tls.enabled'];
     let ledgerClient;
+    let ledgerPrivilegedClient;
     if (tlsEnabled) {
-      ledgerClient = new LedgerClient(url,
+      ledgerClient = new LedgerClient(ledgerClientUrl,
+          grpc.credentials.createSsl(Buffer.from(ca, 'utf8')));
+      ledgerPrivilegedClient = new LedgerPrivilegedClient(ledgerClientUrl,
           grpc.credentials.createSsl(Buffer.from(ca, 'utf8')));
     } else {
-      ledgerClient = new LedgerClient(url, grpc.credentials.createInsecure());
+      ledgerClient = new LedgerClient(ledgerClientUrl,
+          grpc.credentials.createInsecure());
+      ledgerPrivilegedClient = new LedgerPrivilegedClient(
+          ledgerPrivilegedClientUrl,
+          grpc.credentials.createInsecure());
     }
-
-    super(ledgerClient, protobuf, properties);
+    const services = {
+      'ledgerClient': ledgerClient,
+      'ledgerPrivileged': ledgerPrivilegedClient,
+    };
+    super(services, protobuf, properties);
   }
 }
 
