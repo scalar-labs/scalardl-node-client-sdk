dc7a2a39bfe8196e626949da0a42b95387a22bc9
Plenty Su <plenty.su@gmail.com>
2019-11-18 14:52:16 +0800

    Update TestFunction

--- test/TestFunction.java
+++ test/TestFunction.java
@@ -1,37 +1,17 @@
 package com.org1.function;
 
-import com.datastax.driver.core.Session;
-import com.org1.function.repository.KeyspaceRepository;
 import com.scalar.ledger.database.MutableDatabase;
-import com.scalar.ledger.exception.ContractContextException;
 import com.scalar.ledger.udf.Function;
 import java.util.Optional;
 import javax.json.JsonObject;
+import com.scalar.database.api.Put;
+import com.scalar.database.io.TextValue;
+import com.scalar.database.io.Key;
 
 public class TestFunction extends Function {
-
-  private KeyspaceRepository schemaRepository;
-  private Session session;
-
-  public void connectCassandra() {
-    CassandraConnector client = new CassandraConnector();
-    client.connect("127.0.0.1", 9042);
-    this.session = client.getSession();
-    schemaRepository = new KeyspaceRepository(session);
-  }
-
   @Override
-  public void invoke(MutableDatabase mutableDatabase, JsonObject contractArgument,
+  public void invoke(MutableDatabase database, JsonObject contractArgument,
       Optional<JsonObject> functionArgument) {
-    connectCassandra();
-
-    if (!functionArgument.isPresent()) {
-      throw new ContractContextException("please set provide a function argument");
-    }
-    String contractArgumentA = contractArgument.getString("a");
-    String functionId = functionArgument.get().getString("function_id");
-    session.execute(
-        "INSERT INTO scalar.asset (id,argument) VALUES('" + functionId + "', '" + contractArgumentA
-            + "');");
+      throw new RuntimeException("hello world");
   }
 }
