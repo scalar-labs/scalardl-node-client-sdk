e6fd7361baf189318ec79ac2bea03cd40b5f1403
scalarindetail <scalarindetail@indetail.co.jp>
2019-11-19 16:00:34 +0900

    Latest changes

--- test/TestFunction.java
+++ test/TestFunction.java
@@ -1,10 +1,11 @@
 package com.org1.function;
 
+import com.scalar.database.api.Put;
+import com.scalar.database.io.Key;
+import com.scalar.database.io.TextValue;
 import com.scalar.ledger.database.MutableDatabase;
-import com.scalar.ledger.exception.ContractContextException;
 import com.scalar.ledger.udf.Function;
 import java.util.Optional;
-import javax.json.Json;
 import javax.json.JsonObject;
 
 public class TestFunction extends Function {
@@ -12,17 +13,8 @@ public class TestFunction extends Function {
   @Override
   public void invoke(MutableDatabase database, JsonObject contractArgument,
       Optional<JsonObject> functionArgument) {
-
-    if (!functionArgument.isPresent()) {
-      throw new ContractContextException("please set a function argument");
-    }
-
-    String assetId = contractArgument.getString("asset_id");
-    String foo = functionArgument.get().getString("foo");
-    int state = contractArgument.getInt("state");
-
-    JsonObject jsonObject = Json.createObjectBuilder().add("asset_id", assetId).add("state", state)
-        .add("foo", foo).build();
-    throw new RuntimeException(jsonObject.toString());
+    Put put = (new Put(new Key(new TextValue("column_a", "bar")))).forNamespace("foo")
+        .forTable("bar");
+    database.put(put);
   }
 }
